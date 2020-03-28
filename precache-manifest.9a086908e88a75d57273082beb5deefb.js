015295254fede9746024d447bf3c2c83734b256b
timardex <timardex@gmail.com>
2020-03-28 21:29:25 +0100

    gh-pages

---
+++ precache-manifest.9a086908e88a75d57273082beb5deefb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3deea7b1d0a59bbd4cfe3921c261cc2b",
+    "url": "/react-roulette/index.html"
+  },
+  {
+    "revision": "b7eb1733e8596a027207",
+    "url": "/react-roulette/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "93cbd2c9823e281aae82",
+    "url": "/react-roulette/static/css/main.1c759a13.chunk.css"
+  },
+  {
+    "revision": "b7eb1733e8596a027207",
+    "url": "/react-roulette/static/js/2.f52d3df0.chunk.js"
+  },
+  {
+    "revision": "7e08df03892269e6e39da8a34916b217",
+    "url": "/react-roulette/static/js/2.f52d3df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93cbd2c9823e281aae82",
+    "url": "/react-roulette/static/js/main.0d20ed52.chunk.js"
+  },
+  {
+    "revision": "c7394aa25618ceb8ee24",
+    "url": "/react-roulette/static/js/runtime-main.0b2dd44a.js"
+  },
+  {
+    "revision": "d2d1ec002bcce317532b9b1da09b07ae",
+    "url": "/react-roulette/static/media/ball-effect.d2d1ec00.mp3"
+  },
+  {
+    "revision": "cd913118514030bdc76ad207b6162e68",
+    "url": "/react-roulette/static/media/chip-effect.cd913118.mp3"
+  },
+  {
+    "revision": "2d65264c6af2a42fc9a1fa7f620dca9d",
+    "url": "/react-roulette/static/media/rotate-device.2d65264c.gif"
+  }
+]);
